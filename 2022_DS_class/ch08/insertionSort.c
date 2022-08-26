652a5cdbc21fac4a263a7f9c15eabcb2f7e2ee2e
seungchae <s2125@e-mirim.hs.kr>
2022-08-26 12:00:00 +0900

    삽입정렬 실습_수정

--- 2022_DS_class/ch08/insertionSort.c
+++ 2022_DS_class/ch08/insertionSort.c
@@ -2,15 +2,22 @@
 
 int main() {
 	int a[] = { 5,17,27,1,8,3,4,20,30,7 };
-	insertionSort(a);
+	int n = sizeof(a) / sizeof(int);
+	insertionSort(a,n);
+
+	printf("»ğÀÔÁ¤·Ä\n");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+	return 0;
 }
 
-insertionSort(int *a) {
-	int i, j, key, n=9;
+insertionSort(int *a, int n) {
+	int i, j, key;
 	for (i = 1; i < n; i++) {
 		key = a[i];
 		for (j = i - 1; j >= 0; j--) {
-			if (a[j] > key) a[j + 1] = key;
+			if (a[j] > key) a[j + 1] = a[j];
 			else break;
 		}
 		a[j + 1] = key;
